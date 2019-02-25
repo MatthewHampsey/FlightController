4c21a2499fd16abf49cec42be5309cfa66e8718b
MatthewHampsey <matthewjohnhampsey@gmail.com>
2019-02-25 22:49:53 +1100

    Add explicit control parameters and tests

--- controllers/dynamic_compensation_qc.cpp
+++ controllers/dynamic_compensation_qc.cpp
@@ -14,4 +14,26 @@ Vector3f PDDynamic::getControlVector(const Vector3f &euler_angles,
   auto control = _K_p * euler_error + _K_d * euler_derivative_error;
   return _I * control + dynamic_compensation_approx;
 }
+
+void PDDynamic::setParameters(float damping_factor_x, float natural_frequency_x,
+                              float damping_factor_y, float natural_frequency_y,
+                              float damping_factor_z,
+                              float natural_frequency_z) {
+  _K_p = Matrix3f{natural_frequency_x * natural_frequency_x, 0.0f, 0.0f, 
+                  0.0f, natural_frequency_y * natural_frequency_y, 0.0f,
+                  0.0f, 0.0f, natural_frequency_z * natural_frequency_z};
+
+  _K_d = Matrix3f{2 * damping_factor_x * natural_frequency_x, 0.0f, 0.0f,
+                  0.0f, 2 * damping_factor_y * natural_frequency_y, 0.0f,
+                  0.0f, 0.0f, 2 * damping_factor_z * natural_frequency_z};
+}
+
+void PDDynamic::setParameters(float damping_factor, float natural_frequency) {
+  setParameters(damping_factor, natural_frequency, damping_factor,
+                natural_frequency, damping_factor, natural_frequency);
+}
+
+Matrix3f PDDynamic::K_p() { return _K_p; }
+
+Matrix3f PDDynamic::K_d() { return _K_d; }
 }
